c6a4be110a73c789a6b25160cf37cfd55f9688e7
koujp <koujp@qq.com>
2017-05-22 17:49:57 +0800

    extend Object.assign for IE

--- src/ui/Cells.js
+++ src/ui/Cells.js
@@ -19,6 +19,17 @@ function _setRenderTo(renderTo) {
     this.renderTo = renderTo;
 
 };
+if(!Object.assign){
+    Object.assign = function (src,target) {
+        if(!target){
+            return src;
+        }
+        Object.keys(target).forEach(function (key) {
+            src[key] = target[key];
+        });
+        return src;
+    };
+}
 function Cells(cellsModel,config){
 
     init.apply(this,arguments);
